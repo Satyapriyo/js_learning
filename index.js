948dedfd752bc4a40cc94bbad6c9fa70b89dda6e
Satyapriyo Biswas <97757641+Satyapriyo@users.noreply.github.com>
2022-07-17 21:50:04 +0530

    this is first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//This is the first day for learning js and this is mainly a setup and introduction to javascript
+
+//## There are thre ways to run js
+//1 -> install node js and run localy
+//2 -> in browser console 
+//3 -> using script tag in HTML 
+// * bonus in replit make a reple and run code online
+//only console.log()😂😂
+console.log(2+2+5)
